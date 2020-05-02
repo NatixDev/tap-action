4f0aae31450701d20969fd56929b155a454898b9
NatixDev <paul.brumder@gmail.com>
2020-05-02 18:44:49 +0200

    feat: add action-board item

---
+++ src/main/java/com/natix/tapaction/ModEventSubscriber.java
@@ -0,0 +1,28 @@
+package com.natix.tapaction;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+@Mod.EventBusSubscriber(modid = TapActionMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventSubscriber {
+    @SubscribeEvent
+    public static void onRegisterItems(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(
+                setup(new Item(new Item.Properties()), "action-board")
+        );
+    }
+
+    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
+        return setup(entry, new ResourceLocation(TapActionMod.MOD_ID, name));
+    }
+
+    public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final ResourceLocation registryName) {
+        entry.setRegistryName(registryName);
+        return entry;
+    }
+}
+
---
+++ src/main/java/com/natix/tapaction/TapActionMod.java
@@ -0,0 +1,11 @@
+package com.natix.tapaction;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TapActionMod.MOD_ID)
+public class TapActionMod {
+
+    public static final String MOD_ID = "tapaction";
+}
+
+
